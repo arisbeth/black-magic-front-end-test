a7b6e3738a46aea5bb8adfb2a545a16a99800718
Arisbeth <arisbeth.hg@gmail.com>
2021-04-23 17:42:09 -0500

    _First Commit

---
+++ javascripts/functions.js
@@ -0,0 +1,14 @@
+// Only Js
+
+let mylogo = document.getElementById('mobileMenu');
+
+document.getElementById('btnMobileMenu').onclick = function(){
+	mylogo.classList.toggle('fadeMobileMenu');
+}
+
+document.getElementById('closeMobileMenu').onclick = function(){
+	mylogo.classList.toggle('fadeMobileMenu');
+}
+
+
+
